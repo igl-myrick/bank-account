b8bba5c7ce4af72503db51b3999c4ac144b64ad1
igl-myrick <igl.myrick@gmail.com>
2024-02-21 15:55:29 -0800

    add deposit prototype

--- js/scripts.js
+++ js/scripts.js
@@ -3,3 +3,7 @@ function Account(userName, initialDeposit) {
   this.userName = userName;
   this.balance = initialDeposit;
 }
+
+Account.prototype.deposit = function(depositAmt) {
+  this.balance = this.balance + depositAmt;
+}
