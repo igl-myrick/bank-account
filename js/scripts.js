58cb2b808f39450a7ef8fe0e3cd28ef6433cf6d1
igl-myrick <igl.myrick@gmail.com>
2024-02-26 14:29:57 -0800

    add form submission functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 // Account business logic
-function Account(userName, initialDeposit) {
-  this.userName = userName;
-  this.balance = initialDeposit;
+function Account() {
+  this.userName = "";
+  this.balance = 0;
 }
 
 Account.prototype.deposit = function(depositAmt) {
@@ -11,3 +11,31 @@ Account.prototype.deposit = function(depositAmt) {
 Account.prototype.withdraw = function(withdrawalAmt) {
   this.balance = this.balance - withdrawalAmt;
 }
+
+// business logic
+let newAccount = new Account();
+
+// ui logic
+function handleRegistration(e) {
+  e.preventDefault();
+
+  const userName = document.getElementById("name").value;
+  const initialDeposit = document.getElementById("initial-deposit").value;
+  newAccount.userName = userName;
+  newAccount.balance = initialDeposit;
+
+  document.querySelector("span#user-name").innerText = userName;
+  document.querySelector("span#user-balance").innerText = initialDeposit;
+
+  const hiddenElements = document.querySelectorAll(".hidden");
+  hiddenElements.forEach(function(element) {
+    element.classList.remove("hidden")
+  });
+
+  const registrationForm = document.getElementById("registration")
+  registrationForm.remove();
+}
+
+window.addEventListener("load", function() {
+  document.querySelector("form#registration-form").addEventListener("submit", handleRegistration);
+});
